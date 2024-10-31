be4fe8a5bd0e2a213e88d9f248fd350a877a0c67
Sufyaan Sauri <115418185+Sufyaansauri@users.noreply.github.com>
2024-10-31 11:28:19 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gcr/screens/home__screen.dart';
+import 'package:gcr/screens/splash__screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -21,8 +21,8 @@ class App extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      // home: const SplashScreen(),
-      home: const HomeScreen(),
+       home: const SplashScreen(),
+      // home: const HomeScreen(),
     );
   }
 }
